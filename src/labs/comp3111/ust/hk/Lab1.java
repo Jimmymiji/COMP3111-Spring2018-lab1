525e44cf14748afe2fc74b2eafc304ece23db98c
Jimmymiji <jimmybobo12306@gmail.com>
2018-02-10 15:19:09 +0800

    add congratulation

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -4,5 +4,6 @@ public class Lab1 {
      public static void main (String[] args)
      {
     	 System.out.print("Lab1:Introduction to Git and Github");
+    	 System.out.print("congratulations: you learn git basics");
      }
 }
