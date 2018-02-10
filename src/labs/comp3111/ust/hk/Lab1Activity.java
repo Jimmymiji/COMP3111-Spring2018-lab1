c0d8f141706951e6589cf60f551d4703fd9d8616
Jimmymiji <jimmybobo12306@gmail.com>
2018-02-10 15:32:51 +0800

    implement sum of 10 numbers

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -1,5 +1,12 @@
 package labs.comp3111.ust.hk;
 
 public class Lab1Activity {
-
+     public static void main(String[] arg)
+     {
+    	 int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    	 int sum = 0;
+    	 for(int i = 0;i<10;i++)
+    		 sum = sum + arr[i];
+    	 System.out.print(String.format("The sum of the numbers is %d",sum));
+     }
 }
